150a84dfa3e4a408be58fb015b5717d46578eea9
arslanakhtar87 <160528184+arslanakhtar87@users.noreply.github.com>
2024-04-04 01:50:21 +0500

    push my colourfull todolist

---
+++ app.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let condition = true;
+console.log(chalk.bgBlue("\n \t Welcome to code with arslan akhtar - todo-list\n"));
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.green("Enter your new task :")
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(chalk.red.bold(`${addTask.task} Task add in todo-List successfully`));
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addMore",
+            type: "confirm",
+            message: chalk.bgGray("Do you want to add more task ?"),
+            default: "false"
+        }
+    ]);
+    condition = addMoreTask.addMore;
+}
+console.log(chalk.greenBright.bold("your updated todolist:", todoList));
